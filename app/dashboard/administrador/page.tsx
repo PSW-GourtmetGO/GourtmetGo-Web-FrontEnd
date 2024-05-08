2682aafc5614a21c74c7b004b6926bf99043b212
Anthony Solis <asolis7378@uta.edu.ec>
2024-05-07 22:56:18 -0500

    Se coloco la tabla para la busqueda de empleados

--- app/dashboard/administrador/page.tsx
+++ app/dashboard/administrador/page.tsx
@@ -3,10 +3,11 @@ import { BiSearch } from 'react-icons/bi'
 
 const AdministradorPage = () => {
   return (
-    <div style={{
-      backgroundImage: 'url("/imagenes/fondo.svg")',
-      minHeight: "calc(88vh)",
-    }}  >
+    <div className="bg-cover bg-no-repeat bg-center"
+      style={{
+        backgroundImage: 'url("/imagenes/fondo.svg")',
+        minHeight: "calc(80vh)",
+      }}>
       <div className='grid grid-cols-5 grid-rows-5' >
         <div className='col-start-1 col-span-2 text-[45px] border-b border-black' style={{ fontFamily: "David Libre" }}><h1>Empleados</h1></div>
 
@@ -20,10 +21,21 @@ const AdministradorPage = () => {
           <button className='bg-[#01AE67] hover:bg-teal-700 text-white font-bold py-3 px-2 rounded-lg'>Añadir Empleado</button>
         </div>
 
-        <div className='row-start-5'>
+        <div className='row-start-5 col-span-5'>
+          <table className='w-full border-collapse'>
+            <thead className='border-b border-black'>
+              <tr>
+                <th className='py-2 px-4 bg-transparent text-gray-800'>Nombre</th>
+                <th className='py-2 px-4 bg-transparent text-gray-800'>Apellido</th>
+                <th className='py-2 px-4 bg-transparent text-gray-800'>Correo</th>
+                <th className='py-2 px-4 bg-transparent text-gray-800'>Acciones</th>
+              </tr>
+            </thead>
 
 
+          </table>
         </div>
+
 
 
       </div>
