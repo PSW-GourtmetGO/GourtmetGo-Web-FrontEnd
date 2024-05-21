e5df5e725013b690ef1816dd21b881bed67f0117
Anthony Solis <asolis7378@uta.edu.ec>
2024-05-21 00:52:16 -0500

    Correcion de diseño pagina de empleados

--- app/dashboard/administrador/componentes/modalActualizarEmpleado/page.tsx
+++ app/dashboard/administrador/componentes/modalActualizarEmpleado/page.tsx
@@ -7,13 +7,13 @@ import "./page.scss";
 import NoImagen from '../../../../../public/imagenes/noimagen.svg'
 import ActualizarImgane from '../../../../../public/imagenes/actualizarImagen.svg'
 
-interface ModalProps {
+interface ModalUpdate {
     isOpen: boolean;
     onClose: () => void;
 }
 //
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
+const ModalUpdate: React.FC<ModalUpdate> = ({ isOpen, onClose }) => {
     const [formData, setFormData] = useState({
         cedula: '',
         nombre: '',
@@ -51,83 +51,6 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
         console.log("Imagen seleccionada:", e.target.files[0]);
     };
 
-    /*
-    <div className="contenedorPrincipal fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
-                    <div className="contenedor bg-white rounded-lg p-10">
-                        <div className='grid grid-cols-2 w-full gap-x-6'>
-                            <div className='col-start-1'>
-                                <h2 className="text-2xl font-bold mb-4">Registro Empleados</h2>
-                                <form onSubmit={handleSubmit}>
-                                    <div className="mb-4">
-                                        <label htmlFor="cedula" className="block text-gray-700 font-bold mb-2">Cédula:</label>
-                                        <input type="text" id="cedula" name="cedula" value={formData.cedula} onChange={handleInputChange} className="w-full rounded block mb-2 text-black border border-black" required />
-                                    </div>
-                                    <div className="mb-4">
-                                        <label htmlFor="nombre" className="block text-gray-700 font-bold mb-2">Nombre:</label>
-                                        <input type="text" id="nombre" name="nombre" value={formData.nombre} onChange={handleInputChange} className="w-full rounded block mb-2 text-black border border-black" required />
-                                    </div>
-                                    <div className="mb-4">
-                                        <label htmlFor="apellido" className="block text-gray-700 font-bold mb-2">Apellido:</label>
-                                        <input type="text" id="apellido" name="apellido" value={formData.apellido} onChange={handleInputChange} className="w-full rounded block mb-2 text-black border border-black" required />
-                                    </div>
-                                    <div className="mb-4">
-                                        <label htmlFor="fecha_Nacimiento" className="block text-gray-700 font-bold mb-2">Fecha de Nacimiento:</label>
-                                        <input type="date" id="fecha_Nacimiento" name="fecha_Nacimiento" value={formData.fecha_Nacimiento} onChange={handleInputChange} className="w-full rounded block mb-2 text-black border border-black" required />
-                                    </div>
-                                    <div className="mb-4">
-                                        <label htmlFor="direccion" className="block text-gray-700 font-bold mb-2">Dirección:</label>
-                                        <input type="text" id="direccion" name="direccion" value={formData.direccion} onChange={handleInputChange} className="w-full rounded block mb-2 text-black border border-black" required />
-                                    </div>
-                                    <div className="mb-4">
-                                        <label htmlFor="telefono" className="block text-gray-700 font-bold mb-2">Teléfono:</label>
-                                        <input type="text" id="telefono" name="telefono" value={formData.telefono} onChange={handleInputChange} className="w-full rounded block mb-2 text-black border border-black" required />
-                                    </div>
-                                    <div className="mb-4">
-                                        <label htmlFor="correo" className="block text-gray-700 font-bold mb-2">Correo:</label>
-                                        <input type="email" id="correo" name="correo" value={formData.correo} onChange={handleInputChange} className="w-full rounded block mb-2 text-black border border-black" required />
-                                    </div>
-                                    <div className="mb-4">
-                                        <label htmlFor="contrasenia" className="block text-gray-700 font-bold mb-2">Contraseña:</label>
-                                        <input type="password" id="contrasenia" name="contrasenia" value={formData.contrasenia} onChange={handleInputChange} className="w-full rounded block mb-2 text-black border border-black" required />
-                                    </div>
-                                </form>
-                            </div>
-                            <div className="mx-auto w-64 text-center col-start-2 mt-[65%] 2xl:mt-[50%]">
-                                <div className="card">
-                                    <label htmlFor="foto" className="block text-gray-700 font-bold mb-2">Seleccione su Imagen:</label>
-                                    <input
-                                        type="file"
-                                        id="foto"
-                                        name="foto"
-                                        className="w-full rounded block mb-2"
-                                        required
-                                        onChange={handleFileChange}
-                                        ref={inputFileRef}
-                                        style={{ display: 'none' }}
-                                    />
-                                    <div className="relative w-64 text-black border border-black">
-                                        <Image className="w-64 h-64 rounded-full absolute" src={NoImagen} alt="" />
-                                        <div className="w-64 h-64 group hover:bg-gray-200 opacity-60 absolute flex justify-center items-center cursor-pointer transition duration-500" onClick={handleImageClick}>
-                                            <Image className="hidden group-hover:block w-12" src={ActualizarImgane} alt="" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-
-                            <div className="col-span-2 text-right">
-                                <button type="submit" className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-md">Guardar</button>
-                                <button type="button" onClick={onClose} className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded-md ml-2">Cancelar</button>
-                            </div>
-
-
-                        </div>
-
-
-                    </div>
-                </div>
-    */
-
     return (
         <>
             {isOpen && (
@@ -135,7 +58,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
                     <div className="contenedorTarjeta">
                         <div className="tarjeta">
                             <div className="encabezado">
-                                <h1>Registro Empleados</h1>
+                                <h1>Actualizar Empleados</h1>
                             </div>
                             <div className="contenido">
                                 <div className='formulario'>
@@ -236,4 +159,4 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
     );
 };
 
-export default Modal;
+export default ModalUpdate;
--- app/dashboard/administrador/page.tsx
+++ app/dashboard/administrador/page.tsx
@@ -6,19 +6,32 @@ import { BiPencil, BiSearch } from 'react-icons/bi'
 import { FaRegTrashAlt } from 'react-icons/fa'
 import empleado1 from '../../../public/imagenes/mujer2.svg'
 import Modal from './componentes/modalempleado/page';
+import ModalUpdate from './componentes/modalActualizarEmpleado/page';
+import "./page.scss";
 
 const AdministradorPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
 
   // Función para abrir el modal
   const handleModalOpen = () => {
     setIsModalOpen(true);
   };
+    // Función para cerrar el modal
+    const handleModalClose = () => {
+      setIsModalOpen(false);
+    };
 
-  // Función para cerrar el modal
-  const handleModalClose = () => {
-    setIsModalOpen(false);
+  const handleModalUpdateOpen = () => {
+    setIsModalUpdateOpen(true);
   };
+
+  const handleModalUpdateClose = () => {
+    setIsModalUpdateOpen(false);
+  }
+
+
+  
 
 
   return (
@@ -29,35 +42,39 @@ const AdministradorPage = () => {
         minHeight: "calc(80vh)",
       }}
     >
-      <div className="grid grid-cols-5 grid-rows-5 pr-[10%]">
-        <div
-          className="col-start-1 col-span-2 text-[45px] border-b border-black"
-          style={{ fontFamily: "David Libre", height: "70px" }}
-        >
-          <h1>Empleados</h1>
-        </div>
+      <div className='contenedorEncabezado'>
+        <div className="titulo">
+          <div
+            className="text-[45px] border-b border-black"
+            style={{ fontFamily: "David Libre", height: "70px" }}
+          >
+            <h1>Empleados</h1>
+          </div>
 
-        <div
-          className="col-start-4 col-span-2 text-[45px] flex  justify-end "
-          style={{ fontFamily: "David Libre" }}
-        >
-          <h1>Papi Pollos</h1>
+          <div
+            className="text-[45px] flex justify-end "
+            style={{ fontFamily: "David Libre" }}
+          >
+            <h1>Papi Pollos</h1>
+          </div>
         </div>
+        <div className="buscador">
+          <div className="relative ">
+            <input
+              className="w-[320px] 2xl:w-[320px] text-white font-bold bg-[#274C5B] py-3 pl-12 rounded-lg"
+              placeholder="Buscar nombre del empleado"
+            ></input>
+            <BiSearch className="absolute left-3 top-4 text-white" />
+          </div>
+          <div className="flex items-start justify-end mr-[5%] ">
+            <button className="bg-[#01AE67] hover:bg-teal-700 text-white font-bold py-3 px-2 rounded-lg" onClick={handleModalOpen}>
+              Añadir Empleado
+            </button>
+          </div>
+        </div>
+      </div>
 
-        <div className="row-start-2 relative ">
-          <input
-            className="w-[320px] 2xl:w-[320px] text-white font-bold bg-[#274C5B] py-3 pl-12 rounded-lg"
-            placeholder="Buscar nombre del empleado"
-          ></input>
-          <BiSearch className="absolute left-3 top-4 text-white" />
-        </div>
-        
-        <div className="row-start-2 col-start-2 flex items-start justify-end mr-[5%] ">
-          <button className="bg-[#01AE67] hover:bg-teal-700 text-white font-bold py-3 px-2 rounded-lg" onClick={handleModalOpen}>
-            Añadir Empleado
-          </button>
-        </div>
-
+      <div className="grid col-start-1 col-span-2 pr-[10%]">
         <div className="row-start-3 col-span-5 mt-5">
           <table className="w-full border-collapse">
             <thead className="border-b border-black">
@@ -97,7 +114,7 @@ const AdministradorPage = () => {
                 <td className="py-2 px-4">juan@example.com</td>
                 <td className="py-2 px-4">
                   {/* Aquí puedes agregar botones u otras acciones */}
-                  <button className="bg-[#274C5B] text-white p-2 rounded-full">
+                  <button className="bg-[#274C5B] text-white p-2 rounded-full" onClick={handleModalUpdateOpen}>
                     <BiPencil className="left-3 top-4 text-white text-xl 2xl:text-2xl " />
                   </button>
                   <button className="bg-[#B80808] text-white p-2 ml-5 rounded-full">
@@ -113,6 +130,7 @@ const AdministradorPage = () => {
 
 
         <Modal isOpen={isModalOpen} onClose={handleModalClose} />
+        <ModalUpdate isOpen={isModalUpdateOpen} onClose={handleModalUpdateClose} />
 
 
       </div>
