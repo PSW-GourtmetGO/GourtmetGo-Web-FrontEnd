7d88759dd09143e884a401cbc3feb4cfd3b48104
ChristianCLop <christian.chico@hotmail.com>
2024-05-24 14:27:34 -0500

    Validadores Menu

--- app/dashboard/menu/componentes/modalmenu/page.tsx
+++ app/dashboard/menu/componentes/modalmenu/page.tsx
@@ -2,8 +2,8 @@
 import Image from 'next/image';
 import React, { useState, ChangeEvent, FormEvent, useRef, useEffect } from 'react';
 import axios from 'axios';
-import NoImagen from '../../../../../public/imagenes/noimagen.svg'
-import ActualizarImgane from '../../../../../public/imagenes/actualizarImagen.svg'
+import NoImagen from '../../../../../public/imagenes/noimagen.svg';
+import ActualizarImagen from '../../../../../public/imagenes/actualizarImagen.svg';
 
 interface ModalProps {
     isOpen: boolean;
@@ -25,37 +25,37 @@ interface ModalProps {
         accion: string;
     }>>;
 }
+
 interface Categoria {
     id: number;
     nombre: string;
     ver: string;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose,Datos,setData}) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, Datos, setData }) => {
     const [categorias, setCategorias] = useState<Categoria[]>([]);
-
     const [formData, setFormData] = useState({
-        id:0,
+        id: 0,
         nombre: '',
         precio: 0,
         visible: "true",
-        categoria:1
+        categoria: 1
     });
 
     useEffect(() => {
         const obtenerCategorias = async () => {
-          try {
-            const response = await axios.get(`http://localhost:4500/api/Web/categoria/${localStorage.getItem('restauranteID')}`);
-            console.log(response.data)
-            setCategorias(response.data);
-          } catch (error) {
-            console.error('Error al obtener las caracteristicas:', error);
-          }
+            try {
+                const response = await axios.get(`http://localhost:4500/api/Web/categoria/${localStorage.getItem('restauranteID')}`);
+                console.log(response.data);
+                setCategorias(response.data);
+            } catch (error) {
+                console.error('Error al obtener las características:', error);
+            }
         };
         obtenerCategorias();
-      }, []);
+    }, []);
 
-    const guardarPlato = async () =>{
+    const guardarPlato = async () => {
         try {
             const response = await axios.post(`http://localhost:4500/api/Web/plato/${formData.categoria}`, {
                 nombre: formData.nombre,
@@ -63,27 +63,26 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose,Datos,setData}) => {
                 ver: formData.visible,
                 imagen: null
             });
-            alert("plato creado de manera exitosa")            
+            alert("Plato creado de manera exitosa");
         } catch (error) {
-            alert("Hubo un problema con el servidor")
+            alert("Hubo un problema con el servidor");
         }
-    }
+    };
 
-    const actualizarPlato = async (plato:any) =>{
+    const actualizarPlato = async (plato: any) => {
         try {
             const response = await axios.put(`http://localhost:4500/api/Web/plato/${Datos.platoID}`, {
                 nombre: Datos.platoNombre,
                 precio: Datos.precio,
                 ver: Datos.visible,
                 imagen: null,
-                categoria:Datos.categoria
+                categoria: Datos.categoria
             });
-            alert("plato actualizado de manera exitosa")            
+            alert("Plato actualizado de manera exitosa");
         } catch (error) {
-            alert("Hubo un problema con el servidor")
+            alert("Hubo un problema con el servidor");
         }
-
-    }
+    };
 
     const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
         const { name, value } = e.target;
@@ -100,7 +99,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose,Datos,setData}) => {
             [name]: value
         });
     };
-    
+
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         setFormData({
@@ -114,6 +113,24 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose,Datos,setData}) => {
         setData({
             ...Datos,
             [name]: value
+        });
+    };
+
+    const handleNombreChangeEdit = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        const lettersOnly = value.replace(/[^a-zA-Z\s]/g, '');
+        setData({
+            ...Datos,
+            [name]: lettersOnly
+        });
+    };
+
+    const handleNombreChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        const lettersOnly = value.replace(/[^a-zA-Z\s]/g, '');
+        setFormData({
+            ...formData,
+            [name]: lettersOnly
         });
     };
 
@@ -134,6 +151,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose,Datos,setData}) => {
         // Aquí puedes manejar la lógica para cargar la imagen seleccionada
         console.log("Imagen seleccionada:", e.target.files[0]);
     };
+
     return (
         <>
             {(isOpen && Datos.accion === "crear") ? (
@@ -145,7 +163,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose,Datos,setData}) => {
                                 <form onSubmit={handleSubmit}>
                                     <div className="mb-4">
                                         <label htmlFor="nombre" className="block text-gray-700 font-bold mb-2">Nombre:</label>
-                                        <input type="text" id="nombre" name="nombre" value={formData.nombre} onChange={handleInputChange} className="w-full rounded block mb-2 text-black border border-black" required />
+                                        <input type="text" id="nombre" name="nombre" value={formData.nombre} onChange={handleNombreChange} className="w-full rounded block mb-2 text-black border border-black" required />
                                     </div>
                                     <div className="mb-4">
                                         <label htmlFor="costo" className="block text-gray-700 font-bold mb-2">Costo:</label>
@@ -161,10 +179,9 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose,Datos,setData}) => {
                                     <div className='"mb-4'>
                                         <label htmlFor="costo" className="block text-gray-700 font-bold mb-2">Categoria</label>
                                         <select className="w-full rounded block mb-2 text-black border border-black" name="categoria" value={formData.categoria.toString()} onChange={handleSelectChange}>
-                                        {categorias.map((categoria: Categoria, index: number) => (
-                                            <option value={categoria.id}>{categoria.nombre}</option>
+                                            {categorias.map((categoria: Categoria, index: number) => (
+                                                <option value={categoria.id}>{categoria.nombre}</option>
                                             ))};
-
                                         </select>
                                     </div>
                                 </form>
@@ -185,88 +202,81 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose,Datos,setData}) => {
                                     <div className="relative w-64 text-black border border-black">
                                         <Image className="w-64 h-64 rounded-full absolute" src={NoImagen} alt="" />
                                         <div className="w-64 h-64 group hover:bg-gray-200 opacity-60 absolute flex justify-center items-center cursor-pointer transition duration-500" onClick={handleImageClick}>
-                                            <Image className="hidden group-hover:block w-12" src={ActualizarImgane} alt="" />
+                                            <Image className="hidden group-hover:block w-12" src={ActualizarImagen} alt="" />
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
                             <div className="col-start-1 text-left">
                                 <button type="submit" onClick={guardarPlato} className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-md">Guardar</button>
                                 <button type="button" onClick={onClose} className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded-md ml-2">Cerrar</button>
                             </div>
-
                         </div>
-
                     </div>
                 </div>
             ) : (isOpen) ? (
                 <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
-                <div className="bg-white w-[45%] h-[60%] 2xl:w-[45%] 2xl:h-[60%] rounded-lg p-10">
-                    <div className='grid grid-cols-2 w-full gap-x-6'>
-                        <div className='col-start-1'>
-                            <h2 className="text-2xl font-bold mb-4">Editar Plato</h2>
-                            <form onSubmit={handleSubmit}>
-                                <div className="mb-4">
-                                    <label htmlFor="nombre" className="block text-gray-700 font-bold mb-2">Nombre:</label>
-                                    <input type="text" id="platoNombre" name="platoNombre" value={Datos.platoNombre} onChange={handleInputChangeEdit} className="w-full rounded block mb-2 text-black border border-black" required />
-                                </div>
-                                <div className="mb-4">
-                                    <label htmlFor="costo" className="block text-gray-700 font-bold mb-2">Costo:</label>
-                                    <input type="number" id="precio" name="precio" value={Datos.precio} onChange={handleInputChangeEdit} className="w-full rounded block mb-2 text-black border border-black" required />
-                                </div>
-                                <div className='"mb-4'>
-                                    <label htmlFor="costo" className="block text-gray-700 font-bold mb-2">Estado:</label>
-                                    <select className="w-full rounded block mb-2 text-black border border-black" name="visible" value={Datos.visible.toString()} onChange={handleSelectChangeEdit}>
-                                        <option value="true" selected={Datos.visible === "true"}>Visible</option>
-                                        <option value="false" selected={Datos.visible === "false"}>No Visible</option>
-                                    </select>
-                                </div>
-                                <div className='"mb-4'>
-                                    <label htmlFor="costo" className="block text-gray-700 font-bold mb-2">Categoria</label>
-                                    <select className="w-full rounded block mb-2 text-black border border-black" name="categoria" value={Datos.categoria.toString()} onChange={handleSelectChangeEdit}>
-                                    {categorias.map((categoria: Categoria, index: number) => (
-                                        <option value={categoria.id} selected={Datos.categoria === categoria.id}>{categoria.nombre}</option>
-                                        ))};
-
-                                    </select>
-                                </div>
-                            </form>
-                        </div>
-                        <div className="mx-auto w-64 text-center col-start-2 mt-[10%] 2xl:mt-[5%]">
-                            <div className="card">
-                                <label htmlFor="foto" className="block text-gray-700 font-bold mb-2">Seleccione su Imagen:</label>
-                                <input
-                                    type="file"
-                                    id="foto"
-                                    name="foto"
-                                    className="w-full rounded block mb-2"
-                                    required
-                                    onChange={handleFileChange}
-                                    ref={inputFileRef}
-                                    style={{ display: 'none' }}
-                                />
-                                <div className="relative w-64 text-black border border-black">
-                                    <Image className="w-64 h-64 rounded-full absolute" src={NoImagen} alt="" />
-                                    <div className="w-64 h-64 group hover:bg-gray-200 opacity-60 absolute flex justify-center items-center cursor-pointer transition duration-500" onClick={handleImageClick}>
-                                        <Image className="hidden group-hover:block w-12" src={ActualizarImgane} alt="" />
+                    <div className="bg-white w-[45%] h-[60%] 2xl:w-[45%] 2xl:h-[60%] rounded-lg p-10">
+                        <div className='grid grid-cols-2 w-full gap-x-6'>
+                            <div className='col-start-1'>
+                                <h2 className="text-2xl font-bold mb-4">Editar Plato</h2>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-4">
+                                        <label htmlFor="nombre" className="block text-gray-700 font-bold mb-2">Nombre:</label>
+                                        <input type="text" id="platoNombre" name="platoNombre" value={Datos.platoNombre} onChange={handleNombreChangeEdit} className="w-full rounded block mb-2 text-black border border-black" required />
+                                    </div>
+                                    <div className="mb-4">
+                                        <label htmlFor="costo" className="block text-gray-700 font-bold mb-2">Costo:</label>
+                                        <input type="number" id="precio" name="precio" value={Datos.precio} onChange={handleInputChangeEdit} className="w-full rounded block mb-2 text-black border border-black" required />
+                                    </div>
+                                    <div className='"mb-4'>
+                                        <label htmlFor="costo" className="block text-gray-700 font-bold mb-2">Estado:</label>
+                                        <select className="w-full rounded block mb-2 text-black border border-black" name="visible" value={Datos.visible.toString()} onChange={handleSelectChangeEdit}>
+                                            <option value="true" selected={Datos.visible === "true"}>Visible</option>
+                                            <option value="false" selected={Datos.visible === "false"}>No Visible</option>
+                                        </select>
+                                    </div>
+                                    <div className='"mb-4'>
+                                        <label htmlFor="costo" className="block text-gray-700 font-bold mb-2">Categoria</label>
+                                        <select className="w-full rounded block mb-2 text-black border border-black" name="categoria" value={Datos.categoria.toString()} onChange={handleSelectChangeEdit}>
+                                            {categorias.map((categoria: Categoria, index: number) => (
+                                                <option value={categoria.id} selected={Datos.categoria === categoria.id}>{categoria.nombre}</option>
+                                            ))};
+                                        </select>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="mx-auto w-64 text-center col-start-2 mt-[10%] 2xl:mt-[5%]">
+                                <div className="card">
+                                    <label htmlFor="foto" className="block text-gray-700 font-bold mb-2">Seleccione su Imagen:</label>
+                                    <input
+                                        type="file"
+                                        id="foto"
+                                        name="foto"
+                                        className="w-full rounded block mb-2"
+                                        required
+                                        onChange={handleFileChange}
+                                        ref={inputFileRef}
+                                        style={{ display: 'none' }}
+                                    />
+                                    <div className="relative w-64 text-black border border-black">
+                                        <Image className="w-64 h-64 rounded-full absolute" src={NoImagen} alt="" />
+                                        <div className="w-64 h-64 group hover:bg-gray-200 opacity-60 absolute flex justify-center items-center cursor-pointer transition duration-500" onClick={handleImageClick}>
+                                            <Image className="hidden group-hover:block w-12" src={ActualizarImagen} alt="" />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <div className="col-start-1 text-left">
+                                <button type="submit" onClick={() => actualizarPlato(1)} className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-md">Actualizar</button>
+                                <button type="button" onClick={onClose} className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded-md ml-2">Cerrar</button>
+                            </div>
                         </div>
-
-                        <div className="col-start-1 text-left">
-                            <button type="submit" onClick={()=>actualizarPlato(1)} className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-md">Actualizar</button>
-                            <button type="button" onClick={onClose} className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded-md ml-2">Cerrar</button>
-                        </div>
-
                     </div>
-
                 </div>
-            </div>
-              ) : (
+            ) : (
                 <h1>.</h1>
-              )}
+            )}
         </>
     );
 };
