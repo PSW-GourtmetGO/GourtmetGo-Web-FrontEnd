b546099e716a7f30d28a62f1ac09102ba45334a4
Eduardlink <eduardopila2001@outlook.es>
2024-05-24 21:18:13 -0500

    Correccion modal empleados

--- app/dashboard/administrador/componentes/modalActualizarEmpleado/page.tsx
+++ app/dashboard/administrador/componentes/modalActualizarEmpleado/page.tsx
@@ -63,11 +63,15 @@ const ModalUpdate: React.FC<ModalUpdate> = ({ isOpen, onClose, Datos, setDatos }
         <div>
             {isOpen && (
                 <div className="contenedorModal">
-                        <div className="contenedorTarjeta">
-                            <div className="tarjeta">
-                                <div className="encabezado">
-                                    <h1>Actualizar Empleados</h1>
-                                </div>
+                            <div className="tarjetaModal">
+                            <div className="encabezadoModal">
+                            <h1>Actualizar empleado</h1>
+                            <button type="button" onClick={() => {
+                                onClose();
+                            }} className="btnAtras">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z" /></svg>
+                            </button>
+                        </div>
                                 <div className="contenido">
                                     <div className='formulario'>
                                         <form onSubmit={handleSubmit}>
@@ -124,13 +128,11 @@ const ModalUpdate: React.FC<ModalUpdate> = ({ isOpen, onClose, Datos, setDatos }
                                     </div>
                                 </div>
                                 <div className="botones">
-                                    <button type="submit" onClick={actualizarEmpleado} className="boton boton1">Guardar</button>
-                                    <button type="button" onClick={onClose} className="boton boton2">Cancelar</button>
+                                    <button type="submit" onClick={actualizarEmpleado} className="botonVerde">Guardar</button>
                                 </div>
 
 
                             </div>
-                        </div>
                     </div>
             )}
         </div>
